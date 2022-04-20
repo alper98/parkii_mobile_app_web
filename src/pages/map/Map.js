e1dc6a31ffd577e89299407d202efd3a3b753cb2
Alper Altay <alpe0022@stud.kea.dk>
2022-04-20 21:25:17 +0200

    zones

--- src/pages/map/Map.js
+++ src/pages/map/Map.js
@@ -13,12 +13,33 @@ export default function Map() {
   const [zoom, setZoom] = useState(16);
 
   useEffect(() => {
+    console.log(zones);
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
       zoom: zoom,
+    });
+    map.current.on("load", () => {
+      map.current.addSource("parking-zones", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: zones.features,
+        },
+      });
+
+      map.current.addLayer({
+        id: "park-boundary",
+        type: "fill",
+        source: "parking-zones",
+        paint: {
+          "fill-color": "#023E8A",
+          "fill-opacity": 0.4,
+        },
+        filter: ["==", "$type", "Polygon"],
+      });
     });
   });
 
