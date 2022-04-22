8570b1c4f2cff13666b3e83f897f6e11c0db8060
Alper Altay <alpe0022@stud.kea.dk>
2022-04-22 20:54:49 +0200

    fixed prod builds

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Camera from "./pages/camera/Camera";
-import MapComponent from "./pages/map/MapComponent";
+import MapContainer from "./pages/map/MapContainer";
 import Profile from "./pages/profile/Profile";
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
 
 function App() {
   return (
@@ -17,7 +20,7 @@ function App() {
             <Route path="/" element={<Navigate to="/camera" />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/camera" element={<Camera />} />
-            <Route path="/map" element={<MapComponent />} />
+            <Route path="/map" element={<MapContainer />} />
           </Routes>
         </Grid>
         <Grid item xs={1}>
