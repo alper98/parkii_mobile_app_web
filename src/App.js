f81ded7959be6e927176953ffafbac53a21453a9
Alper Altay <alpe0022@stud.kea.dk>
2022-04-23 09:13:40 +0200

    fixed height issues in mobile browsers

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ import Footer from "./components/footer/Footer";
 import Camera from "./pages/camera/Camera";
 import MapContainer from "./pages/map/MapContainer";
 import Profile from "./pages/profile/Profile";
-
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
+import { use100vh } from "react-div-100vh";
 
 function App() {
+  const height = use100vh();
+  const realDeviceHeight = height ? height / 1.08 : "50vh";
   return (
     <BrowserRouter>
-      <Grid container direction="column" height={"100vh"}>
+      <Grid container direction="column">
         <Grid item xs={11}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/camera" />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/camera" element={<Camera />} />
-            <Route path="/map" element={<MapContainer />} />
-          </Routes>
+          <div style={{ height: realDeviceHeight }}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/camera" />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/camera" element={<Camera />} />
+              <Route path="/map" element={<MapContainer />} />
+            </Routes>
+          </div>
         </Grid>
         <Grid item xs={1}>
           <Footer />
