70a73a6576398da454360e21bea7e35e1816f431
Alper Altay <alpe0022@stud.kea.dk>
2022-04-27 19:01:33 +0200

    removed a test

--- src/userContext.js
+++ src/userContext.js
@@ -5,17 +5,17 @@ import { isAuthenticated } from "./api/auth/authService";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  // const [user, setUser] = useState(undefined);
-  const [user, setUser] = useState({
-    id: 2,
-    name: "Alper",
-    email: "alper@test.dk",
-    email_verified_at: null,
-    created_at: "2022-04-27T13:13:17.000000Z",
-    updated_at: "2022-04-27T13:13:17.000000Z",
-    phone: null,
-    car: null,
-  });
+  const [user, setUser] = useState(undefined);
+  // const [user, setUser] = useState({
+  //   id: 2,
+  //   name: "Alper",
+  //   email: "alper@test.dk",
+  //   email_verified_at: null,
+  //   created_at: "2022-04-27T13:13:17.000000Z",
+  //   updated_at: "2022-04-27T13:13:17.000000Z",
+  //   phone: null,
+  //   car: null,
+  // });
 
   useEffect(() => {
     const checkLoggedIn = async () => {
