d8ae0f30e15eaf9c596561a71fb723f63ffa12fc
Alper Altay <alpe0022@stud.kea.dk>
2022-04-29 09:03:04 +0200

    camera work

--- src/pages/camera/CameraPage.js
+++ src/pages/camera/CameraPage.js
@@ -1,47 +1,25 @@
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import { useCallback, useRef, useState } from "react";
-import Webcam from "react-webcam";
+import { useState } from "react";
+import Camera, { FACING_MODES, IMAGE_TYPES } from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
 import ImagePreview from "./components/ImagePreview";
 
 export default function CameraPage() {
-  const webcamRef = useRef(null);
   const { height, width } = useWindowDimensions();
   const [image, setImage] = useState("");
-
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImage(imageSrc);
-  }, [webcamRef]);
-
-  const videoConstraints = {
-    width: width,
-    height: height * 0.8,
-    facingMode: "environment",
-  };
 
   return (
     <div className="webcamCapture">
       {image === "" ? (
         <>
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
+          <Camera
+            imageType={IMAGE_TYPES.JPG}
+            idealFacingMode={FACING_MODES.ENVIRONMENT}
+            idealResolution={{ width: width, height: height * 0.94 }}
+            onTakePhoto={(dataUri) => {
+              setImage(dataUri);
+            }}
           />
-          <Box textAlign="center" padding={2}>
-            <Button
-              size="large"
-              onClick={capture}
-              variant="contained"
-              color="success"
-              style={{ width: "50%" }}
-            >
-              Scan
-            </Button>
-          </Box>
         </>
       ) : (
         <ImagePreview image={image} setImage={setImage} />
